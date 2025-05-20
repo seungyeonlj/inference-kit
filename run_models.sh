6fc1cee9e6334920e5c70f8178d7c94b568d6b62
seungyeonlj <amyjwa@gmail.com>
2025-05-20 08:56:55 +0000

    Update directory structure

--- run_models.sh
+++ run_models.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=$(pwd):$PYTHONPATH
-python src/models/vision_language.py --config configs/molmo.yaml
-python src/models/vision_language.py --config configs/llava-onevision.yaml
-python src/models/vision_language.py --config configs/qwen2_5_vl.yaml
+# python src/run_vllm_vision_language.py --config configs/molmo.yaml
+python src/run_vllm_vision_language.py --config configs/llava-onevision.yaml
+# python src/run_vllm_vision_language.py --config configs/qwen2_5_vl.yaml
