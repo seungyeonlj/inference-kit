462e76a9db6921929c0be49a97f891ae858b8160
seungyeonlj <amyjwa@gmail.com>
2025-05-29 08:34:13 +0000

    Add scripts

---
+++ prepare_benchmarks.sh
@@ -0,0 +1,4 @@
+# multimodal rewardbnech
+curl -sL https://raw.githubusercontent.com/facebookresearch/multimodal_rewardbench/refs/heads/main/scripts/0_download_data.py | python
+# -> images saved to ./data
+
--- run_models.sh
+++ run_models.sh
@@ -1,4 +1,5 @@
 export PYTHONPATH=$(pwd):$PYTHONPATH
-# python src/run_vllm_vision_language.py --config configs/molmo.yaml
+python src/run_vllm_vision_language.py --config configs/molmo.yaml
 python src/run_vllm_vision_language.py --config configs/llava-onevision.yaml
-# python src/run_vllm_vision_language.py --config configs/qwen2_5_vl.yaml
+python src/run_vllm_vision_language.py --config configs/qwen2_5_vl.yaml
+python src/run_api.py --config configs/gpt-41.yaml
